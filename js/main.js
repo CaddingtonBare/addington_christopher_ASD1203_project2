045de00d76bc7eb6036f8ec9fd9dc6516ccd8af8
Christopher Addington <caddington@fullsail.edu>
2012-03-06 20:16:08 -0500

    Added three data types into xhr folder

--- js/main.js
+++ js/main.js
@@ -172,6 +172,7 @@ $(function(){
     var clearLocal = function(){
         if(localStorage.length === 0){
             alert("There is no data to clear.");
+            window.location.reload();
         }else{
             localStorage.clear();
             alert("All teams have been deleted!");
