016019e443dcc3f7d08734f068a95491595294cc
Christopher Addington <caddington@fullsail.edu>
2012-03-06 23:53:16 -0500

    Created first part of dynamic page population script in test.js

--- js/test.js
+++ js/test.js
@@ -5,6 +5,30 @@
 
 $(function(){
 
-
+    var getJSON = function(){
+        $.ajax({
+            url: 'xhr/data.json',
+            type: 'GET',
+            dataType: 'json',
+            success: function(json){
+                /*
+                <div data-role="collapsible" data-collapsed="true" data-theme="a">
+		    <h3><img src="images/Basketball_landing.png" />Basketball</h3>
+                    <div data-role="content">
+			<ul data-role="listview" data-theme="c">
+			    <li><a href="#bball1">json</a></li>
+			    <li><a href="#bball2">Charity</a></li>
+			    <li><a href="#bball3">20-somethings</a></li>
+			    <li><a href="#bball4">Half-court dunkers</a></li>
+			</ul>
+		    </div>
+		</div>
+                */
+                
+            },
+        });
+    };
+    
+console.log(json.team);
 
 });
