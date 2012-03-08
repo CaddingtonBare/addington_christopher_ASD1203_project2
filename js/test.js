97cbccbf1b9485bc502f8003dd733ddaf56d088f
Christopher Addington <caddington@fullsail.edu>
2012-03-08 00:02:56 -0500

    Going crazy trying to automatically create data with test.js ajax call & function

--- js/test.js
+++ js/test.js
@@ -5,30 +5,32 @@
 
 $(function(){
 
-    var getJSON = function(){
         $.ajax({
-            url: 'xhr/data.json',
+            url: 'xhr/data.js',
             type: 'GET',
             dataType: 'json',
             success: function(json){
-                /*
-                <div data-role="collapsible" data-collapsed="true" data-theme="a">
-		    <h3><img src="images/Basketball_landing.png" />Basketball</h3>
-                    <div data-role="content">
-			<ul data-role="listview" data-theme="c">
-			    <li><a href="#bball1">json</a></li>
-			    <li><a href="#bball2">Charity</a></li>
-			    <li><a href="#bball3">20-somethings</a></li>
-			    <li><a href="#bball4">Half-court dunkers</a></li>
-			</ul>
-		    </div>
-		</div>
-                */
-                
+                for(var i=0, j=json.teams.length; i<j; i++){
+                    var whichTeam = json.teams[i];
+                    $(
+                        '<div data-role="collapsible" data-collapsed="true" data-theme="a">'+
+                            '<h3><img src="images/Basketball_landing.png" />Basketball</h3>'+
+                            '<div data-role="content">'+
+                                '<ul data-role="listview" data-theme="c">'+
+                                    '<li>' + whichTeam.sports[0] + ' ' + whichTeam.sports[1] + '</li>'+
+                                    '<li>' + whichTeam.teamname[0] + ' ' + whichTeam.teamname[1] + '</li>'+
+                                    '<li>' + whichTeam.teamsize[0] + ' ' + whichTeam.teamsize[1] + '</li>'+
+                                    '<li>' + whichTeam.availabletime[0] + ' ' + whichTeam.availabletime[1] + '</li>'+
+                                    '<li>' + whichTeam.nextdate[0] + ' ' + whichTeam.nextdate[1] + '</li>'+
+                                    '<li>' + whichTeam.notes[0] + ' ' + whichTeam.notes[1] + '</li>'+
+                                '</ul>'+
+                            '</div>'+
+                        '</div>'
+                    ).appendTo('#JSONpage');
+                }
             },
         });
-    };
-    
-console.log(json.team);
-
+        
+console.log(json.teams[0].sports[1]);
+        
 });
