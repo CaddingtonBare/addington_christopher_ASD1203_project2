dba951994cf45be212a17dab986f382f7d21ecdb
Christopher Addington <caddington@fullsail.edu>
2012-03-09 00:47:30 -0500

    Pushed functioning JSON retrieval event from test to live environment

--- js/main.js
+++ js/main.js
@@ -232,20 +232,36 @@ $(function(){
         }
     }
     
-    //Retrieve JSON data
-    var addJSON = function(){
+    //Create eventhandler for clicking "Retrieve JSON" & populate page with the pulled data
+    $('#addJSON').on("click", function(){
         $.ajax({
-            url: 'xhr/data.json',
-            type: 'GET',
-            dataType: 'json',
-            success: function(response){
-                
-            },
-            error: function(result){
-                alert(result);
+            url: "xhr/data.js",
+            type: "GET",
+            dataType: "json",
+            success: function(answer){
+                alert("JSON data retrieved successfully!");
+                for(var i=0, j=answer.teams.length; i<j; i++){
+                    var whichTeam = answer.teams[i];
+                    $(
+                        '<div>'+
+                            '<h3><img src="images/Basketball_landing.png" />Basketball</h3>'+
+                            '<div>'+
+                                '<ul>'+
+                                    '<li>' + whichTeam.sports[0] + ' ' + whichTeam.sports[1] + '</li>'+
+                                    '<li>' + whichTeam.teamname[0] + ' ' + whichTeam.teamname[1] + '</li>'+
+                                    '<li>' + whichTeam.teamsize[0] + ' ' + whichTeam.teamsize[1] + '</li>'+
+                                    '<li>' + whichTeam.availabletime[0] + ' ' + whichTeam.availabletime[1] + '</li>'+
+                                    '<li>' + whichTeam.nextdate[0] + ' ' + whichTeam.nextdate[1] + '</li>'+
+                                    '<li>' + whichTeam.notes[0] + ' ' + whichTeam.notes[1] + '</li>'+
+                                '</ul>'+
+                            '</div>'+
+                        '</div>'
+                    ).appendTo('#jsontent');   
+                }
             }
         });
-    }
+                
+    });
     
     
     var errMsg = $('#errors');
--- xhr/data.js
+++ xhr/data.js
@@ -2,10 +2,10 @@
     "teams":[
         {
         "sports":           ["Sport:", "Basketball"],
-        "teamname":         ["Team Name:", "Hoop da Whoos"],
+        "teamname":         ["Team Name:", "Hoop da Whoop"],
         "teamsize":         ["Team Size:", "4"],
         "availabletime":    ["Only evening games:", "Yes"],
-        "nextdate":         ["Next available date:", "2011-11-05"],
+        "nextdate":         ["Next available date:", "3/25/2012"],
         "notes":            ["Notes:", "I can tell from the pixels"]
         },
         {
@@ -13,7 +13,7 @@
         "teamname":         ["Team Name:", "Charity"],
         "teamsize":         ["Team Size:", "2"],
         "availabletime":    ["Only evening games:", "No"],
-        "nextdate":         ["Next available date:", "2011-11-02"],
+        "nextdate":         ["Next available date:", "4/15/2012"],
         "notes":            ["Notes:", "Me Scott 2v2 tourney"]
         },
         {
@@ -21,7 +21,7 @@
         "teamname":         ["Team Name:", "20-somethings"],
         "teamsize":         ["Team Size:", "5"],
         "availabletime":    ["Only evening games:", "Yes"],
-        "nextdate":         ["Next available date:", "2011-11-03"],
+        "nextdate":         ["Next available date:", "5/3/2012"],
         "notes":            ["Notes:", "Base league 25-35 5v5"]
         },
         {
@@ -29,7 +29,7 @@
         "teamname":         ["Team Name:", "Half-court dunkers"],
         "teamsize":         ["Team Size:", "3"],
         "availabletime":    ["Only evening games:", "Yes"],
-        "nextdate":         ["Next available date:", "2011-11-04"],
+        "nextdate":         ["Next available date:", "4/9/2012"],
         "notes":            ["Notes:", "Battle of the businesses summer league 3v3"]
         },
         {
@@ -37,7 +37,7 @@
         "teamname":         ["Team Name:", "Dayton Ballers"],
         "teamsize":         ["Team Size:", "3"],
         "availabletime":    ["Only evening games:", "No"],
-        "nextdate":         ["Next available date:", "2011-11-01"],
+        "nextdate":         ["Next available date:", "6/22/2012"],
         "notes":            ["Notes:", "Me Tom Joe 3v3 tourney"]
         }]
 }
