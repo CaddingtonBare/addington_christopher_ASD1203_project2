b685c801d6b0d715e9a0a1df3ea2b4c8036c836d
Christopher Addington <caddington@fullsail.edu>
2012-03-09 15:04:20 -0500

    Adjusted some html ids for the csv and started building the CSV ajax call

--- js/test.js
+++ js/test.js
@@ -5,72 +5,82 @@
 
 $(function(){
 
-$('#addJSON').on("click", function(){
-    $('#jsontent').empty();
-    $.ajax({
-        url: "xhr/data.js",
-        type: "GET",
-        dataType: "json",
-        success: function(answer){
-            alert("JSON data retrieved successfully!");
-            for(var i=0, j=answer.teams.length; i<j; i++){
-                var whichTeam = answer.teams[i];
-                $(
-                    '<div id="team' + i + '">'+
-                        '<h3><img src="images/Basketball_landing.png" />Basketball</h3>'+
-                        '<div>'+
-                            '<ul>'+
-                                '<li>' + whichTeam.sports[0] + ' ' + whichTeam.sports[1] + '</li>'+
-                                '<li>' + whichTeam.teamname[0] + ' ' + whichTeam.teamname[1] + '</li>'+
-                                '<li>' + whichTeam.teamsize[0] + ' ' + whichTeam.teamsize[1] + '</li>'+
-                                '<li>' + whichTeam.availabletime[0] + ' ' + whichTeam.availabletime[1] + '</li>'+
-                                '<li>' + whichTeam.nextdate[0] + ' ' + whichTeam.nextdate[1] + '</li>'+
-                                '<li>' + whichTeam.notes[0] + ' ' + whichTeam.notes[1] + '</li>'+
-                            '</ul>'+
-                        '</div>'+
-                    '</div>'
-                ).appendTo('#jsontent');   
-            }
-        }
-    });
-});
-        
-$('#addXML').on("click", function(){
-        $('#xmlcontent').empty();
+    $('#addJSON').on("click", function(){
+        $('#jsontent').empty();
         $.ajax({
-        url: 'xhr/data.xml',
-        type: 'GET',
-        dataType: 'xml',
-        success: function(xml){
-            alert("XML data retrieved successfully!");
-            $(xml).find("team").each(function(){
-                var sport = $(this).find('sports').text();
-                var teamname = $(this).find('teamname').text();
-                var teamsize = $(this).find('teamsize').text();
-                var availabletime = $(this).find('availabletime').text();
-                var nextdate = $(this).find('nextdate').text();
-                var notes = $(this).find('notes').text();
-                $('' +
-                    '<div id="team">'+
-                        '<h3><img src="images/' + sport + '_10px.png" />' + sport + '</h3>'+
-                        '<div>'+
-                            '<ul>'+
-                                '<li>Sport: ' + sport + '</li>'+
-                                '<li>' + teamname + '</li>'+
-                                '<li>' + teamsize + '</li>'+
-                                '<li>' + availabletime + '</li>'+                
-                                '<li>' + nextdate + '</li>'+                
-                                '<li>' + notes + '</li>'+
-                            '</ul>'+
-                        '</div>'+
-                    '</div>'
-                ).appendTo('#xmlcontent');
-            })
-        }
+            url: "xhr/data.js",
+            type: "GET",
+            dataType: "json",
+            success: function(answer){
+                alert("JSON data retrieved successfully!");
+                for(var i=0, j=answer.teams.length; i<j; i++){
+                    var whichTeam = answer.teams[i];
+                    $(
+                        '<div id="team' + i + '">'+
+                            '<h3><img src="images/Basketball_landing.png" />Basketball</h3>'+
+                            '<div>'+
+                                '<ul>'+
+                                    '<li>' + whichTeam.sports[0] + ' ' + whichTeam.sports[1] + '</li>'+
+                                    '<li>' + whichTeam.teamname[0] + ' ' + whichTeam.teamname[1] + '</li>'+
+                                    '<li>' + whichTeam.teamsize[0] + ' ' + whichTeam.teamsize[1] + '</li>'+
+                                    '<li>' + whichTeam.availabletime[0] + ' ' + whichTeam.availabletime[1] + '</li>'+
+                                    '<li>' + whichTeam.nextdate[0] + ' ' + whichTeam.nextdate[1] + '</li>'+
+                                    '<li>' + whichTeam.notes[0] + ' ' + whichTeam.notes[1] + '</li>'+
+                                '</ul>'+
+                            '</div>'+
+                        '</div>'
+                    ).appendTo('#jsontent');   
+                }
+            }
+        });
+    });
+        
+    $('#addXML').on("click", function(){
+            $('#xmlcontent').empty();
+            $.ajax({
+            url: 'xhr/data.xml',
+            type: 'GET',
+            dataType: 'xml',
+            success: function(xml){
+                alert("XML data retrieved successfully!");
+                $(xml).find("team").each(function(){
+                    var sport = $(this).find('sports').text();
+                    var teamname = $(this).find('teamname').text();
+                    var teamsize = $(this).find('teamsize').text();
+                    var availabletime = $(this).find('availabletime').text();
+                    var nextdate = $(this).find('nextdate').text();
+                    var notes = $(this).find('notes').text();
+                    $('' +
+                        '<div id="team">'+
+                            '<h3><img src="images/' + sport + '_10px.png" />' + sport + '</h3>'+
+                            '<div>'+
+                                '<ul>'+
+                                    '<li>Sport: ' + sport + '</li>'+
+                                    '<li>' + teamname + '</li>'+
+                                    '<li>' + teamsize + '</li>'+
+                                    '<li>' + availabletime + '</li>'+                
+                                    '<li>' + nextdate + '</li>'+                
+                                    '<li>' + notes + '</li>'+
+                                '</ul>'+
+                            '</div>'+
+                        '</div>'
+                    ).appendTo('#xmlcontent');
+                })
+            }
+        })
+    });
+    
+    $('#addCSV').on("click", function(){
+        ('#csvcontent').empty();
+        $.ajax({
+            url: 'xhr/data.csv',
+            type: 'GET',
+            dataType: 'xml',
+            success: function(csv){
+                alert("CSV data retrieved successfully!");
+            }
+        })
+    
+    
     })
-});
-
-$('#addCSV')
-
-
 });
